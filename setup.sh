f25ebb2979e6a548a9a8bc80dddb2d2d484f1753
rigvedhenryharvin <156786301+rigvedhenryharvin@users.noreply.github.com>
2024-02-13 14:41:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 echo "[general]
-email = \"ralhanprateek@gmail.com\"
+email = \"rigved.sarougi@henryharvin.com\"
 " > ~/.streamlit/credentials.toml
 echo "[server]
 headless = true
